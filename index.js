839c41cb4f5ea5f1572c97921b1e8a9a755ecd04
ikiK <kiki.ikik@gmail.com>
2021-05-28 17:14:07 +0200

    algogoritam gotov?

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log(true)
+document.querySelector('#test').addEventListener('click', e => {
+  //document.querySelector('input').value
+  const arr = [5,3,9,8]
+  let i = 1
+  const loop = arr.length - i
+  let result = []
+
+  for (i; i < loop; i++) {
+    //console.log(arr[i])
+    const right = arr.slice(i + 1)
+    const left = arr
+      .slice()
+      .reverse()
+      .slice(-i)
+
+    if (sumarr(right) === sumarr(left)) {
+      result.push(i)
+    }
+    document.querySelector('#result').innerHTML = result
+  }
+})
+
+let sumarr = ar => {
+  let res = 0
+  ar.forEach(s => {
+    res += s
+  })
+  return res
+}
